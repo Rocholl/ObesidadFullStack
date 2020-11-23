c17266cfc3f836d12b7d3fe123256cd8223979cf
Rocholl <55140973+Rocholl@users.noreply.github.com>
2020-11-23 19:10:38 +0000

    Lunes23

--- obesidadsql.sql
+++ obesidadsql.sql
@@ -21,7 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `obesidadsql`
 --
-
+create database obesidadsql;
+use obesidadsql;
 -- --------------------------------------------------------
 
 --
